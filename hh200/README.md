8f83378acf56818f66ddf01fd6bb3fda50ad024f
Reza <rezahandzalah@gmail.com>
2025-04-07 07:24:06 +0700

    erl_interface

--- hh200/README.md
+++ hh200/README.md
@@ -10,10 +10,40 @@ its parser implementation (a [handwritten](https://github.com/Orange-OpenSource/
 - happy == 2.1.4
 
 ### Development
-Adding a rule to the grammar is a 3-step process. As an illustration:
+Developing a rule in the grammar is an activity of conservatively editing `src/L.x` and `src/P.y` at the following sites.
+
+```haskell
+-- src/L.x
+tokens :-
+    ...
+
+data Token =
+    ...
+  deriving (Eq, Show)
 ```
+```haskell
+-- src/P.y
+%token
+    ...  { ... }
+
+rule : ...
 ```
 ``` sh
-stack purge
+stack purge  # rm -rf .stack-work
 stack run
 ```
+
+## Erlang runtime
+### Development
+The shell might interfere with how we interact with `/bin/erl_call` sometimes, for example with the
+out-of-context message "erl_call:.:{N}: not enough arguments". Use another shell or look closer to
+what your shell does every step of the way.
+
+```sh
+erl -sname hh200 -setcookie $(cat ~/.erlang.cookie)
+epmd -names
+erl_call -sname hh200 -a 'init stop'
+```
+erl_call -n hh200 -c $(cat ~/.erlang.cookie) -e rt:dbg().
+erl_call -sname hh200 -a 'rt dbg []'
+erl_call -sname hh200 -c $(cat ~/.erlang.cookie) -a 'rt dbg []'
--- hh200/src/Hh200/Cli.hs
+++ hh200/src/Hh200/Cli.hs
@@ -2,23 +2,26 @@ module Hh200.Cli
     ( cli
     ) where
 
+import System.Process (readProcess)
+import System.Exit (ExitCode(..))
+import Control.Exception (catch, SomeException)
+
 import System.IO (readFile)
 import Control.Monad (foldM)
 import Data.ByteString.Char8 (pack)
 import Data.Version (showVersion)
 import Options.Applicative
 
-import Paths_hh200
--- import Hh200.Etf (wr)
+import qualified Paths_hh200 (version)
 import Hh200.Etf
 import Hh200.Types
 import L
--- import P ( Expr(..) )
-import P
+-- import P
 
 data Args = Args
     { source  :: Maybe String
     , version :: Bool
+    , call :: Bool
     }
 
 cli :: IO ()
@@ -32,6 +35,9 @@ cli = go =<< execParser opts where
                   <> short 'V'
                   <> help "Print version info and exit" )
 
+        <*> switch ( long "call"
+                  <> short 'C'
+                  <> help "??" )
 
 simpleRequest :: String
 -- simpleRequest = "GET https://example.com\nHTTP 200\n"
@@ -43,33 +49,57 @@ simpleRequest = "HTTP/1.1 200\n"  -- ok
 
 -- simpleRequest = "get https://example.com\n"  -- ok
 
+erlRepl :: [String] -> String -> IO ()
+erlRepl args replInput = do
+    res <- catch
+        (readProcess "erl" args replInput)
+        (\e -> return $ "[Hh200.Cli] " ++ show (e :: SomeException))
+    putStrLn res
+
+erlCall :: [String] -> IO ()
+erlCall args = do
+    let replInputNone = ""
+    res <- catch
+        (readProcess "erl_call" args replInputNone)
+        (\e -> return $ "[Hh200.Cli] " ++ show (e :: SomeException))
+    putStrLn res
+
 
 go :: Args -> IO ()
 
 -- hh200 --version
-go (Args _ True) = putStrLn $ showVersion Paths_hh200.version
+go Args { version = True } = putStrLn $ showVersion Paths_hh200.version
 
--- hh200 /home/tbmreza/gh/hh200/examples/hello.hhs
-go (Args (Just s) _) = do
-    let o = "output.etf"
-    let tokensOrPanic = alexScanTokens simpleRequest
-    let tokens = tokensOrPanic
-    print tokens
+-- hh200 --call
+-- $ erl_call -sname hh200 -a 'init stop'
+go Args { call = True } =
+    -- erlCall ["-sname", "hh200", "-a", "init stop"]
+    -- erlCall ["-sname", "hh200", "-setcookie", "ZTDCNMUDFQEYFLUVJUAT"]
+    -- erlCall ["-sname", "hh200"]
+    -- erlRepl ["-sname", "hh200"] "{."
+    erlCall ["-sname", "hh200", "-a", "rt dbg"]
 
-    -- -- ??: send erlang
-    -- program <- parseFile s
-    -- print program
-    --
-    -- where
-    --
-    -- -- parseFile :: FilePath -> IO [Statement]
-    -- -- parseFile filePath = do
-    -- --     content <- readFile filePath
-    -- --     let ast = parse $ alexScanTokens content :: E [Statement]
-    -- --     case ast of
-    -- --         Ok stmts -> return stmts
-    -- --         Failed msg -> return []
-    -- parseFile :: FilePath -> IO String
-    -- parseFile filePath = do
-    --     content <- readFile filePath
-    --     return content
+-- -- hh200 /home/tbmreza/gh/hh200/examples/hello.hhs
+-- go (Args (Just s) _) = do
+--     let o = "output.etf"
+--     let tokensOrPanic = alexScanTokens simpleRequest
+--     let tokens = tokensOrPanic
+--     print tokens
+--
+--     -- -- ??: send erlang
+--     -- program <- parseFile s
+--     -- print program
+--     --
+--     -- where
+--     --
+--     -- -- parseFile :: FilePath -> IO [Statement]
+--     -- -- parseFile filePath = do
+--     -- --     content <- readFile filePath
+--     -- --     let ast = parse $ alexScanTokens content :: E [Statement]
+--     -- --     case ast of
+--     -- --         Ok stmts -> return stmts
+--     -- --         Failed msg -> return []
+--     -- parseFile :: FilePath -> IO String
+--     -- parseFile filePath = do
+--     --     content <- readFile filePath
+--     --     return content
