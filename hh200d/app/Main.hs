c2ca4a18651b710fadaa959b1e88af0ce75298c6
Reza <rezahandzalah@gmail.com>
2025-12-30 14:56:05 +0700

    tk

--- hh200d/app/Main.hs
+++ hh200d/app/Main.hs
@@ -24,11 +24,20 @@ import Language.LSP.Protocol.Types
 import Language.LSP.Protocol.Message
 import Control.Monad.IO.Class
 import qualified Data.Text.IO as T
+import qualified Data.Text as Text
+import Data.Aeson hiding (defaultOptions)
+
+data Config = Config { verbose :: Bool }
+  deriving (Show)
+
+instance FromJSON Config where
+  parseJSON = withObject "Config" $ \v ->
+    Config <$> v .:? "verbose" .!= False
 
 validate :: Uri -> [Diagnostic]
 validate _ = []
 
-handlers :: Handlers (LspM ())
+handlers :: Handlers (LspM Config)
 handlers = mconcat
   [ notificationHandler SMethod_Initialized $ \_not -> do
       liftIO $ T.putStrLn "Server initialized"
@@ -39,17 +48,25 @@ handlers = mconcat
         PublishDiagnosticsParams uri Nothing diags
   ]
 
+handleConfig cfg = do
+    liftIO $ T.putStrLn $ "Config changed: " <> Text.pack (show cfg)
+    pure ()
+
 main :: IO ()
 main = do
-  _ <- runServer $ ServerDefinition
-    { defaultConfig = ()
-    , configSection = "hh200d"
-    , parseConfig = const $ const $ Right ()
-    , onConfigChange = const $ pure ()
-    , doInitialize = \env _req -> pure $ Right env
-    , staticHandlers = \_caps -> handlers
-    , interpretHandler = \env -> Iso (runLspT env) liftIO
-    -- , options = defaultOptions {optServerInfo = Just (ServerInfo "??: this compiles but isn't effective at setting server doc at LspInfo" (Just "runtime red wall dismissable, server still attached"))}
-    , options = defaultOptions
-    }
-  pure ()
+    _int <- runServer $
+        ServerDefinition
+          { defaultConfig = Config False
+          , configSection = "hh200d"
+          , parseConfig = \_old v ->
+                case fromJSON v of
+                  Success newConfig -> Right newConfig
+                  Error e -> Left (Text.pack e)
+          , onConfigChange = handleConfig
+          , doInitialize = \env _req -> pure $ Right env
+          , staticHandlers = \_caps -> handlers
+          , interpretHandler = \env -> Iso (runLspT env) liftIO
+          -- , options = defaultOptions {optServerInfo = Just (ServerInfo "??: this compiles but isn't effective at setting server doc at LspInfo" (Just "runtime red wall dismissable, server still attached"))}
+          , options = defaultOptions
+          }
+    pure ()
