3f54ff03c338b8e8b5d09c5c08fdcdf4e899e3ce
Reza <rezahandzalah@gmail.com>
2025-12-31 08:06:09 +0700

    tk

--- hh200d/app/Main.hs
+++ hh200d/app/Main.hs
@@ -48,15 +48,28 @@ handlers = mconcat
       sendNotification SMethod_TextDocumentPublishDiagnostics $
         PublishDiagnosticsParams uri Nothing diags
   , notificationHandler SMethod_WorkspaceDidChangeConfiguration $ \_msg -> do
+      -- (auto) This handler satisfies the LSP client that the notification is recognized.
+      -- The actual configuration update is managed by the library via `onConfigChange`.
       liftIO $ T.putStrLn "Received workspace/didChangeConfiguration"
       return ()
   ]
 
--- | This function is called when the client sends a `workspace/didChangeConfiguration` notification.
+-- (auto) This callback is invoked by the library (via `onConfigChange`) when the 
+-- configuration is updated, typically following a `workspace/didChangeConfiguration` 
+-- notification and successful parsing.
 handleConfig :: Config -> LspM Config ()
 handleConfig cfg = do
     liftIO $ T.putStrLn $ "Config changed: " <> Text.pack (show cfg)
     pure ()
+
+sh :: a -> Handlers (LspM Config)
+sh caps = handlers
+
+-- configResult :: a -> Value -> Either Text a
+configResult old v =
+    case fromJSON v of
+        Success newConfig -> Right newConfig
+        Error e -> Left (Text.pack e)
 
 main :: IO ()
 main = do
@@ -64,13 +77,15 @@ main = do
         ServerDefinition
           { defaultConfig = Config False
           , configSection = "hh200d"
-          , parseConfig = \_old v ->
-                case fromJSON v of
-                  Success newConfig -> Right newConfig
-                  Error e -> Left (Text.pack e)
+          , parseConfig = configResult
+          -- , parseConfig = \_old v ->
+          --       case fromJSON v of
+          --         Success newConfig -> Right newConfig
+          --         Error e -> Left (Text.pack e)
           , onConfigChange = handleConfig
           , doInitialize = \env _req -> pure $ Right env
-          , staticHandlers = \_caps -> handlers
+          -- , staticHandlers = \_caps -> handlers
+          , staticHandlers = sh
           , interpretHandler = \env -> Iso (runLspT env) liftIO
           -- , options = defaultOptions {optServerInfo = Just (ServerInfo "??: this compiles but isn't effective at setting server doc at LspInfo" (Just "runtime red wall dismissable, server still attached"))}
           , options = defaultOptions
