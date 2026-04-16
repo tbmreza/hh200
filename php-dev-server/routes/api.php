ff88eab76df396158a1314a64be53bb747046055
Reza <rezahandzalah@gmail.com>
2026-04-16 10:56:19 +0700

    tk

--- php-dev-server/routes/api.php
+++ php-dev-server/routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EchoController;
+use App\Http\Controllers\XlsUploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::any('/echo', EchoController::class);
+Route::post('/xls', XlsUploadController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
--- php-dev-server/routes/web.php
+++ php-dev-server/routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\EchoController;
-use App\Http\Controllers\XlsUploadController;
 use Illuminate\Support\Facades\Route;
-
-Route::any('/echo', EchoController::class);
-Route::post('/xls', XlsUploadController::class);
 
 Route::get('/', function () {
     return view('welcome');
